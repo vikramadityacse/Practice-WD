bb1b78d9ca169a236498fc62e35e9ec30bd01209
vikram.aditya.cse@gmail.com <158454679+vikramadityacse@users.noreply.github.com>
2025-06-19 22:43:22 +0530

    signup component

--- week-14/14.3-offline/app/api/user/route.ts
+++ week-14/14.3-offline/app/api/user/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 //app.get("api/user", function(){
 //    res.json({
@@ -7,11 +7,26 @@ import { NextResponse } from "next/server";
 //      })      
 //})
 
-// Similarly 
 
-export function GET(){
+export function GET(req : NextRequest){
+    // do validation check
+    // fetch data from db
     return NextResponse.json({
         email: "vikramaditya@gmail.com",
         name: "Vikramaaditya"
+    })
+}
+
+export async function POST(req : NextRequest){
+    //body :- 
+    const body = await req.json();
+    //headers :-
+    console.log(req.headers.get("authorization"))
+    //queryParams :-
+    console.log(req.nextUrl.searchParams.get("name"))
+    console.log(body)
+    // hit db with username and password 
+    return NextResponse.json({
+        message : "You are Signed In"
     })
 }
