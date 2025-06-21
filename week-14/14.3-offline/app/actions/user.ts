3ed7c0d5870c92e2ce76345244c3829d23320157
vikram.aditya.cse@gmail.com <158454679+vikramadityacse@users.noreply.github.com>
2025-06-21 21:10:45 +0530

    added user.ts

---
+++ week-14/14.3-offline/app/actions/user.ts
@@ -0,0 +1,19 @@
+"use server"
+
+// server actions
+
+import client from "@/db"
+
+export async function signup(email: string, password: string){
+    try{
+        await client.user.create({
+            data : {
+                email : email,
+                password : password
+            }
+        })
+        return true
+    } catch(e){
+        return false
+    }
+}
