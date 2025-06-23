0232915cde0c60cf4cf4f2671b4316fcce734dff
vikram.aditya.cse@gmail.com <158454679+vikramadityacse@users.noreply.github.com>
2025-06-23 23:41:53 +0530

    23rd june

---
+++ week-15/week-15-live-1-main/src/index.ts
@@ -0,0 +1,31 @@
+
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+console.log("vikram")
+
+const app = express();
+app.use(express.json());
+
+const client = new PrismaClient();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Healthy server"
+    })
+})
+
+app.post("/", async (req, res) => {
+    await client.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name
+        }
+    })
+
+    res.json({
+        message: "Done signing up!"
+    })
+})
+
+app.listen(3000);
