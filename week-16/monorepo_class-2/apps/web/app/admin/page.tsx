7de397f0a592280e33ac48e7f2cf08c6b05bea66
vikram.aditya.cse@gmail.com <158454679+vikramadityacse@users.noreply.github.com>
2025-08-22 23:56:18 +0530

    updates

--- week-16/monorepo_class-2/apps/web/app/admin/page.tsx
+++ week-16/monorepo_class-2/apps/web/app/admin/page.tsx
@@ -1,9 +1,11 @@
-import { Admin } from "@repo/ui/admin";
+import { Admin } from "@repo/ui/admin"; 
+import { InputBox } from "@repo/ui/input-box"
 
 
 export default function () {
     return <div>
        hi from admin Page 
        <Admin/>
+       <InputBox/>
     </div>
 }
