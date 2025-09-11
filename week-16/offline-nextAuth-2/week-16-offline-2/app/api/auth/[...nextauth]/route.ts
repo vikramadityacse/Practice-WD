e75c013f58c4eff4f31be0d4d3c73f81ceaf4bfe
vikram.aditya.cse@gmail.com <158454679+vikramadityacse@users.noreply.github.com>
2025-09-11 07:11:41 +0530

    signin and singup page

---
+++ week-16/offline-nextAuth-2/week-16-offline-2/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth from 'next-auth'
+import { NextRequest } from 'next/server';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const handler = NextAuth({
+    providers : [
+        CredentialsProvider({
+            name: "Email",
+            credentials: {
+                username: {label:"Username", type:"text", placeholder: "Email"},
+                password: {label:"Password", type:"password", placeholder:"Password"}
+            },
+            async authorize(credentials: any){
+                const username = credentials.username;
+                const password = credentials.password;
+                const user = await prisma.user.findOne({
+                    where : {
+                        email: username,
+                        password: password
+                    }
+                })
+                return {
+                    id: user.id,
+                    email: user.email
+                }
+            }
+        })
+    ]
+})
+
+export const GET = handler;
+export const POST = handler;
---
+++ week-16/offline-nextAuth-2/week-16-offline-2/app/signup/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1><br /><br />
+      <input type="text" placeholder="name" /><br /><br />
+      <input type="email" placeholder="email" /><br /><br />
+      <input type="password" placeholder="password" /><br /><br /><br />
+      <button>Submit</button>
+    </div>
+  );
+}
