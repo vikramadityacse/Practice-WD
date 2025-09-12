afc7ae3498bf9edef14aec1d5fdc0eb0b6231d92
vikram.aditya.cse@gmail.com <158454679+vikramadityacse@users.noreply.github.com>
2025-09-13 02:42:28 +0530

    stage 1

---
+++ week-17/Paytm-fullstack/apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+
+import {PrismaClient}  from "@repo/db/client";
+
+const client = new PrismaClient();
+export default function Page(){
+  return (
+    <div className="text-2xl">
+      yoyoyo whatsupp whatsuppppp
+    </div>
+  )
+}
